80738bc0537fbaefa2318bcaf08a86500aff651d
Diego Sarmiento <60120140+diegosarmiento11@users.noreply.github.com>
2021-07-17 04:30:24 -0500

    Login and Menulist were added, mediaqueries were added too.

--- robinfood-test/robinfood/src/views/About.vue
+++ robinfood-test/robinfood/src/views/About.vue
@@ -1,5 +1,42 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <div class="about-p-container">
+      <p class="about-p">Pizzerías</p>
+    </div>
+    <h2 class="about-title">Tiendas</h2>
+    <p class="about-p2">Escoge tu pizzería favorita</p>
+    <Menu-cards/>
   </div>
 </template>
+
+
+
+<script>
+
+import MenuCards from "../components/MenuCards.vue";
+
+
+export default {
+  name: "About",
+  components: {
+    MenuCards,
+  }
+}
+</script>
+
+<style lang="scss">
+  .about-p-container {
+    margin: 0 auto;
+    width: 30%;
+  }
+  .about-p {
+    font-size: 26px;
+    border-bottom: 2px solid #f9c444;
+  }
+  .about-title {
+    display: none;
+  }
+  .about-p2{
+
+  }
+</style>
